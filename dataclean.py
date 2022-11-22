d8ab115848c5d36ab11e1dce3c6b57754f8a39d7
Jacob N6eeley <jneeley@DESKTOP-C6TRT88.localdomain>
2022-11-21 20:38:53 -0600

    created btc_gpu_mktData

--- dataclean.py
+++ dataclean.py
@@ -17,6 +17,7 @@ gpu = pd.read_csv(r'C:/Users/Jacne/Documents/python/visualization_and_datamining
 #need prices from 10/1/2021 : 09/30/2022 only
 btc = btc.drop(btc[btc['Unix Epoch Time(Seconds)'] < 1633132800].index)
 btc = btc.drop(btc[btc['Unix Epoch Time(Seconds)'] > 1664582400].index)
+btc = btc.reset_index(drop=True)
 
 #conversions
 prices = []
@@ -32,7 +33,39 @@ gpu['GPU'] = gpus
 
 #drop cards that have a frequency less than 12
 gpu = gpu[gpu.groupby('GPU').GPU.transform('count')>11]#transform function is goofy, i goes by index.
-gpu['GPU'].value_counts()
 
+#inefficient, but works for now. This needs to be dynamic in the Future.
+card1 = pd.DataFrame(gpu[gpu['GPU']=='Radeon RX 6600'])
+card2 = pd.DataFrame(gpu[gpu['GPU']=='Radeon RX 6600 XT'])
+card3 = pd.DataFrame(gpu[gpu['GPU']=='Radeon RX 6700 XT'])
+card4 = pd.DataFrame(gpu[gpu['GPU']=='Radeon RX 6800'])
+card5 = pd.DataFrame(gpu[gpu['GPU']=='Radeon RX 6800 XT'])
+card6 = pd.DataFrame(gpu[gpu['GPU']=='Radeon RX 6900 XT'])
+card7 = pd.DataFrame(gpu[gpu['GPU']=='GeForce RTX 3060 T'])
+card8 = pd.DataFrame(gpu[gpu['GPU']=='GeForce RTX 3070'])
+card9 = pd.DataFrame(gpu[gpu['GPU']=='GeForce RTX 3070 T'])
+card10 = pd.DataFrame(gpu[gpu['GPU']=='GeForce RTX 3080'])
+card11 = pd.DataFrame(gpu[gpu['GPU']=='GeForce RTX 3080 T'])
+card12 = pd.DataFrame(gpu[gpu['GPU']=='GeForce RTX 3090'])
+##################################################################################
 
+clst = [card1,card2,card3,card4,card5,card6
+        ,card7,card8,card9,card10,card11,card12]
+sums = []
+for c in clst:
+    #c.drop(['QTY Sold'],axis=1, inplace = True)
+    sums.append(round((c['eBay Price'].sum()*12)/(365),2))
+    
+headers = []
+itr = 0
+for g in gpu['GPU']:  
+    headers.append(g)
+    itr+=1
+    if itr == 12:
+        break
+    
+dailyAvgPrice = pd.DataFrame([sums]*365,columns = headers)
+btc_gpu = pd.concat([btc,dailyAvgPrice],axis = 1, join='inner')
+export_csv = btc_gpu.to_csv(r'btc_gpu_mktData.csv',index = None, header = True)  
+    
 
