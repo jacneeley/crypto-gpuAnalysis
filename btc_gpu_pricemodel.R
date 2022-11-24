a16fd7da69fca97c3ad2db1c60c3b38747a77627
Jacob N6eeley <jneeley@DESKTOP-C6TRT88.localdomain>
2022-11-23 19:01:30 -0600

    misc changes

--- btc_gpu_pricemodel.R
+++ btc_gpu_pricemodel.R
@@ -7,57 +7,31 @@ summary(resale)
 #Linear Regression Model - Summarize Data
 
 #Most popular mining gpus
-o1<-lm(GeForce.RTX.3090~btc_Price)
+o1<-lm(btc_Price~GeForce.RTX.3090)
 summary(o1)
-o7<-lm(Radeon.RX.6700.XT~btc_Price)
-summary(o7)
-
-#looking at things the other way
-#o2<-lm(btc_Price~GeForce.RTX.3090)
-#summary(o2)
-
-#powerful cards
-o2<-lm(GeForce.RTX.3080.T~btc_Price)
+o2<-lm(btc_Price~Radeon.RX.6700.XT)
 summary(o2)
-
-o3<-lm(GeForce.RTX.3080~btc_Price)
-summary(o3)
-
-o4<-lm(Radeon.RX.6900.XT~btc_Price)
-summary(o4)
-
-o5<-lm(Radeon.RX.6800.XT~btc_Price)
-summary(o5)
-
-o6<-lm(Radeon.RX.6800~btc_Price)
-summary(o6)
-
-#don't think this one is useful, just wanted to see what it would look like.
-o8<-lm(btc_Price~GeForce.RTX.3090)
-summary(o8)
-
-o9<-lm(btc_Price~GeForce.RTX.3090+Radeon.RX.6700.XT)
-summary(o9)
 
 
 #btc relative to gpu
-plot(GeForce.RTX.3090~btc_Price)
+plot(btc_Price~GeForce.RTX.3090)
 plot(o1)
 
-plot(Radeon.RX.6700.XT~btc_Price)
-plot(o7)
-
+plot(btc_Price~Radeon.RX.6700.XT)
+plot(o2)
 
 plot(btc_Price~GeForce.RTX.3090)
+
+o3<-lm(btc_Price~GeForce.RTX.3090+Radeon.RX.6700.XT)
 
 
 #is btc_price a good predictor for a given gpu price?
 p1<-predict(o1)
-p2<-predict(o7)
+p2<-predict(o2)
 
 #residuals
 r1<-residuals(o1)
-r2<-residuals(o7)
+r2<-residuals(o2)
 
 #increasing or decreasing variance?
 plot(r1~p1,xlab="predicted",ylab="Residuals")
